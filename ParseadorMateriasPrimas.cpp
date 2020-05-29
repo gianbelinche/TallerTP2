ea09eb71dad5cfac4fc6705ad5a332191e5be167
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-05-29 15:47:09 -0300

    Añado manejo de errores al parsear

--- ParseadorMateriasPrimas.cpp
+++ ParseadorMateriasPrimas.cpp
@@ -1,6 +1,7 @@
 #include "ParseadorMateriasPrimas.h"
 #include "ColaBloqueante.h"
 #include <string>
+#include <iostream>
 
 ParseadorMateriasPrimas::ParseadorMateriasPrimas(
 ColaBloqueante& cola_agricultor,
@@ -12,18 +13,22 @@ ColaBloqueante& cola_leniador,ColaBloqueante& cola_minero) :
 void ParseadorMateriasPrimas::parsear(const std::string nombre_entrada){
     archivo.open(nombre_entrada);
     std::string linea;
-    while (getline(archivo,linea)){
-        for (unsigned int i = 0; i < linea.length(); i++){
-            if (linea[i] == 'T'){
-                cola_agricultor.encolar(linea[i]);
-            }
-            if (linea[i] == 'M'){
-                cola_leniador.encolar(linea[i]);
-            }
-            if (linea[i] == 'C' || linea[i] == 'H'){
-                cola_minero.encolar(linea[i]);
+    try{
+        while (getline(archivo,linea)){
+            for (unsigned int i = 0; i < linea.length(); i++){
+                if (linea[i] == 'T'){
+                    cola_agricultor.encolar(linea[i]);
+                }
+                if (linea[i] == 'M'){
+                    cola_leniador.encolar(linea[i]);
+                }
+                if (linea[i] == 'C' || linea[i] == 'H'){
+                    cola_minero.encolar(linea[i]);
+                }
             }
         }
+    }catch(...){
+        std::cerr << "Error en el parseo de los archivos\n";
     }
     archivo.close();
 }
--- ParseadorTrabajadores.cpp
+++ ParseadorTrabajadores.cpp
@@ -1,28 +1,34 @@
 #include "ParseadorTrabajadores.h"
 #include <string>
+#include <iostream>
 
 void ParseadorTrabajadores::parsear(const std::string nombre_entrada){
     archivo.open(nombre_entrada);
     std::string linea;
-    while (getline(archivo,linea)){
-        if (linea.find("Agricultores") != std::string::npos){
-            cant_agricultores = std::stoi(linea.substr(13));
+    try{
+        while (getline(archivo,linea)){
+            if (linea.find("Agricultores") != std::string::npos){
+                cant_agricultores = std::stoi(linea.substr(13));
+            }
+            if (linea.find("Leniadores") != std::string::npos){
+                cant_lenadores = std::stoi(linea.substr(11));
+            }
+            if (linea.find("Mineros") != std::string::npos){
+                cant_mineros = std::stoi(linea.substr(8));
+            }
+            if (linea.find("Cocineros") != std::string::npos){
+                cant_cocineros = std::stoi(linea.substr(10));
+            }
+            if (linea.find("Carpinteros") != std::string::npos){
+                cant_carpinteros = std::stoi(linea.substr(12));
+            }
+            if (linea.find("Armeros") != std::string::npos){
+                cant_armeros = std::stoi(linea.substr(8));
+            }
         }
-        if (linea.find("Leniadores") != std::string::npos){
-            cant_lenadores = std::stoi(linea.substr(11));
-        }
-        if (linea.find("Mineros") != std::string::npos){
-            cant_mineros = std::stoi(linea.substr(8));
-        }
-        if (linea.find("Cocineros") != std::string::npos){
-            cant_cocineros = std::stoi(linea.substr(10));
-        }
-        if (linea.find("Carpinteros") != std::string::npos){
-            cant_carpinteros = std::stoi(linea.substr(12));
-        }
-        if (linea.find("Armeros") != std::string::npos){
-            cant_armeros = std::stoi(linea.substr(8));
-        }
+    } catch(...){
+        std::cerr << "Error en el parseo de los archivos\n";
     }
+
     archivo.close();
 }
